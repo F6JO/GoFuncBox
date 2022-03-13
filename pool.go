2c9aaac7c1fbdd38ea712d657fdc37f5b6e7e00f
F6JO <64877810+F6JO@users.noreply.github.com>
2022-03-13 13:45:33 +0800

    upload

---
+++ pool.go
@@ -0,0 +1,40 @@
+type SimplePool struct {
+	wg   sync.WaitGroup
+	work chan func() //任务队列
+}
+
+func NewSimplePoll(workers int) *SimplePool {
+	p := &SimplePool{
+		wg:   sync.WaitGroup{},
+		work: make(chan func()),
+	}
+	p.wg.Add(workers)
+	//根据指定的并发量去读取管道并执行
+	for i := 0; i < workers; i++ {
+		go func() {
+			defer func() {
+				// 捕获异常 防止waitGroup阻塞
+				if err := recover(); err != nil {
+					fmt.Println(err)
+					p.wg.Done()
+				}
+			}()
+			// 从workChannel中取出任务执行
+			for fn := range p.work {
+				fn()
+			}
+			p.wg.Done()
+		}()
+	}
+	return p
+}
+// 添加任务
+func (p *SimplePool) Add(fn func()) {
+	p.work <- fn
+}
+
+// 执行
+func (p *SimplePool) Run() {
+	close(p.work)
+	p.wg.Wait()
+}
